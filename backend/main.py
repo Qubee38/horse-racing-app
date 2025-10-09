3c0cce514768dac69a127ec62428f8f0a59452aa
Qubee38 <41812003+Qubee38@users.noreply.github.com>
2025-10-09 15:13:07 +0900

    allow_originsの設定条件を修正

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # フロントエンドのURL
+    allow_origins=settings.cors_origins,  # フロントエンドのURL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
